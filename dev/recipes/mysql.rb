257e222c4cf7ecf5404f8fcaff4e008d740632cb
yalab <rudeboyjet@gmail.com>
2013-10-18 20:39:11 +0900

    Restart mysql aftesr create my.cnf

--- dev/recipes/mysql.rb
+++ dev/recipes/mysql.rb
@@ -4,4 +4,10 @@ package "libmysql++-dev"
 
 template "/etc/mysql/my.cnf" do
   source "my.cnf.erb"
+  notifies :run, 'execute[restart_mysql]', :immediately
+end
+
+execute "restart_mysql" do
+  command "service mysql restart"
+  action :nothing
 end
