5eb00d4646bf204f935098d93bdf3b66ee4eda7d
yalab <rudeboyjet@gmail.com>
2013-11-04 19:33:17 +0900

    knife cookbook create opensocial -o site-cookbooks

---
+++ opensocial/metadata.rb
@@ -0,0 +1,7 @@
+name             'opensocial'
+maintainer       'oke-ya.inc'
+maintainer_email 'yalab@oke-ya.com'
+license          'MIT'
+description      'Installs/Configures opensocial'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
