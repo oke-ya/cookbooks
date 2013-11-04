38535aba22d94d7c0747e458ad644ab49d9067a7
yalab <rudeboyjet@gmail.com>
2013-11-04 21:10:49 +0900

    create shared/config directory.

--- opensocial/recipes/default.rb
+++ opensocial/recipes/default.rb
@@ -24,6 +24,12 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 node[:deploy].each do |application, deploy|
+  directory "#{deploy[:deploy_to]}/shared/config/initializers" do
+    action :create
+    user deploy[:user]
+    group deploy[:group]
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/initializers/oauth.rb" do
     source "oauth.rb.erb"
     user deploy[:user]
