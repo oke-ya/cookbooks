504a58b935b34018beafa8dca73036d8a29fc03b
yalab <rudeboyjet@gmail.com>
2014-01-09 17:03:29 +0900

    Fixed github::public_key .

--- github/recipes/public_key.rb
+++ github/recipes/public_key.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 include_recipe "github::api"
 
 node['deploy'].each do |application, deploy|
@@ -6,19 +7,36 @@ node['deploy'].each do |application, deploy|
   ruby_block "get_keys" do
     block do
       token = node['github']['token']
-      key_urls = JSON.parse(RestClient.get("https://api.github.com/repos/#{node.default['repo_path']}/collaborators", {'Authorization' => "token #{token}"})).map{|j| j['html_url'] + ".keys"}
-      keys = key_urls.map{|url| RestClient.get(url).split(/\n/).first }
-      keys += IO.read(auth_file_path).split(/\n/)
+      key_urls = nil
+      open("https://api.github.com/repos/#{node.default['repo_path']}/collaborators",  {'Authorization' => "token #{token}"}) do |io|
+        key_urls = JSON.parse(io.read).map{|j| j['html_url'] + ".keys"}
+      end
+      keys = key_urls.map{|url|
+        key = nil
+        open(url) do |io|
+          key = io.read.split(/\n/).first
+        end
+        key
+      }
+      keys += IO.read(auth_file_path).split(/\n/) if File.exists?(auth_file_path)
       keys.compact!
       keys.reject!{|key| key =~ /^\s*$/}
+      keys.uniq!
       node.default["pub_key_file_content"] = keys.join("\n")
     end
-    subscribes :create, resources("cookbook_file[/tmp/oauth_token]")
     action :nothing
   end
 
+  directory File.dirname(auth_file_path) do
+    action :create
+    mode   0600
+    owner  (deploy[:user] || "root")
+    group  (deploy[:user] || "root")
+  end
+  
   file auth_file_path do
     resources("ruby_block[get_keys]").run_action(:run)
     content node.default["pub_key_file_content"]
+    subscribes :create, "directory[#{File.dirname(auth_file_path)}]"
   end
 end
