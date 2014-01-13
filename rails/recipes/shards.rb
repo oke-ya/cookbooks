0ef5f6940e58dbb153cd2559a30c1191a253a284
yalab <rudeboyjet@gmail.com>
2014-01-13 19:30:53 +0900

    add rails::shards.

---
+++ rails/recipes/shards.rb
@@ -0,0 +1,23 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  template "#{deploy[:deploy_to]}/shared/config/shards.yml" do
+    source "shards.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    environments = ['development', 'production'].tap{|_| _ << deploy[:rails_env] }
+    environments.select!{|_| _ && _.length > 0}
+
+    variables(:database      => deploy[:database],
+              :read_replicas => deploy[:read_replicas],
+              :environments   => environments)
+
+   notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+end
