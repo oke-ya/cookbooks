3c50923dd82278d458b4c27030a570853a916bb6
yalab <rudeboyjet@gmail.com>
2014-02-05 12:29:54 +0900

    Nginx add maintenance mode.

--- my-nginx_unicorn/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'my-nginx_unicorn'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures my-nginx_unicorn'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
