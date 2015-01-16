3bb9a7cc92f635c7aced832f87831e75730cf865
yalab <rudeboyjet@gmail.com>
2015-01-16 19:27:50 +0900

    Fixed dotenv.

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -1,15 +1,10 @@
-# https://github.com/mikamai/opsworks-dotenv
-
 require 'shellwords'
-require 'yaml'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
-
-  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
-
-  open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
-    deploy[:app_env].to_h.each do |name, value|
-      f.puts "#{name}=#{value.to_s.shellescape}"
-    end
+  
+  template "#{deploy[:deploy_to]}/shared/.env" do
+    source 'dotenv.erb'
+    mode '0644'
+    variables(:env => deploy[:app_env])
   end
 end
