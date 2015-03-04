149e792082477048406aea5ab296f22b0d2bd94e
yalab <rudeboyjet@gmail.com>
2015-03-04 15:42:42 +0900

    Rename td_agent -> td-agent.

--- fluentd_nginx/metadata.rb
+++ fluentd_nginx/metadata.rb
@@ -5,4 +5,4 @@ license          'MIT'
 description      'Installs/Configures fluentd for nginx log'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'td_agent'
+depends          'td-agent'
