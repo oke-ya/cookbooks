e8977fa2b2b3f89f2d5c2c285e967294481d09bd
yalab <rudeboyjet@gmail.com>
2015-09-27 21:58:37 +0900

    Revert "Remove nginx_repository because its use 1.8.0."
    This reverts commit 03b8080ead7ab13d4c74d9a2c42714f15804efb6.

---
+++ nginx_repository/recipes/default.rb
@@ -0,0 +1,26 @@
+include_recipe 'nginx'
+
+pkgs = %w(nginx nginx-common nginx-full)
+
+apt_repository 'nginx' do
+  uri          'http://ppa.launchpad.net/nginx/stable/ubuntu'
+  distribution node['lsb']['codename']
+  components   ['main']
+  keyserver    'keyserver.ubuntu.com'
+  key          'C300EE8C'
+  deb_src      true
+  pkgs.map{|name| "package[#{name}]"}.each do |pkg|
+    notifies :upgrade, pkg
+  end
+  notifies :create, "directory[/var/log/nginx]"
+end
+
+pkgs.each do |pkg|
+  package pkg do
+    version "1.6.3-1+trusty0" if pkg == "nginx"
+    ignore_failure true
+    action :nothing
+    options '-o Dpkg::Options::="--force-confold"'
+    notifies :restart, "service[nginx]"
+  end
+end
